52030fea1978f2bbf71b17e1a9d8ed20909d822b
RafaGS <RafaGS@users.noreply.github.com>
2026-02-10 19:13:19 +0100

    Add files via upload

---
+++ TangPrimer25KDock/EjemploLED1/src/led1.v
@@ -0,0 +1,15 @@
+// Módulo simple para encender un LED en modo parpadeo
+module led_controller_simple(
+    input clk,
+    output reg led_output
+);
+    
+    reg [31:0] counter;
+    
+    always @(posedge clk) begin
+        counter <= counter + 1;
+        
+        led_output <= counter[23];
+    end
+    
+endmodule
